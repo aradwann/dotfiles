9ad6e2ee7af7a3310345893f38c6d77bdaac7162
ahmedradwann <ahmed.radwan@paytabs.com>
2024-07-03 23:17:36 +0300

    add golang binaries to PATH

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,13 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+# golang 
+set -x GOPATH $HOME/go
+set -x PATH $GOPATH/bin $PATH
 
 # pnpm
 set -gx PNPM_HOME "/home/aradwan/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
