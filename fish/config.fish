58e1ffcc7b86526238335fb24de793974d0f41e0
Ahmed Radwan <aradwann@proton.me>
2024-07-14 04:25:51 +0300

    add git comfig

--- fish/config.fish
+++ fish/config.fish
@@ -2,8 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 # golang 
-set -x GOPATH $HOME/go
-set -x PATH $GOPATH/bin $PATH
+set -gx GOPATH $HOME/go
+set -gx PATH $GOPATH/bin $PATH
 
 # pnpm
 set -gx PNPM_HOME "/home/aradwan/.local/share/pnpm"
@@ -11,3 +11,10 @@ if not string match -q -- $PNPM_HOME $PATH
     set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+set -gx EDITOR hx
+
+# aliases
+alias dnf dnf5
+alias u "sudo dnf upgrade --refresh -y"
+alias i "sudo dnf install"
