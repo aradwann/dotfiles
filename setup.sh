001c48659082ea63fbd5b01e8107a35386051881
Ahmed Radwan <aradwann@proton.me>
2024-07-14 14:32:18 +0300

    add git ssh signing

--- setup.sh
+++ setup.sh
@@ -24,4 +24,27 @@ if [ -d ~/.config/helix ]; then
 fi
 ln -sf ~/dotfiles/helix ~/.config/helix
 
+# Gitconfig
+if [ -f ~/.gitconfig ]; then
+  rm ~/.gitconfig
+fi
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+
+# Setup commit signing with SSH key
+if [ ! -d ~/.config/git ]; then
+  mkdir -p ~/.config/git
+fi
+
+if [ -f ~/.ssh/id_ed25519 ]; then
+  chmod 600 ~/.ssh/id_ed25519
+  # Modify this line to meet your needs.
+  # Declaring the `git` namespace helps prevent cross-protocol attacks.
+  echo "$(git config --get user.email) namespaces=\"git\" $(cat ~/.ssh/id_ed25519.pub)" >> ~/.ssh/allowed_signers
+  chmod 644 ~/.config/git/allowed_signers
+  git config --global gpg.format ssh
+  git config --global user.signingkey ~/.ssh/id_ed25519.pub
+  git config --global gpg.ssh.allowedSignersFile ~/.config/git/allowed_signers
+  ssh-add ~/.ssh/id_ed25519
+fi
+
 echo "Dotfiles have been set up!"
